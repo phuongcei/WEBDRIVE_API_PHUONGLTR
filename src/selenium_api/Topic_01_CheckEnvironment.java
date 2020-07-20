67f3441bc58844ae87fde73b43a4f5b38a49397e
phuongcei <phuongcei@gmail.com>
2020-07-20 23:51:58 +0700

    Add first code.

---
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -0,0 +1,21 @@
+package selenium_api;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_CheckEnvironment {
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void TC_01_CheckEnvironment() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
