22bb335cb56d3482c94bc35716e6e2a402d820bd
trle <trle@trle-7400.internal.synopsys.com>
2020-07-21 09:44:53 +0700

    Update TC01

--- src/selenium_api/Topic_01_CheckEnvironment.java
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -1,21 +1,41 @@
 package selenium_api;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_CheckEnvironment {
+//import junit.framework.Assert;
 
+public class Topic_01_CheckEnvironment {
+	WebDriver driver;
+	
+	
 	@BeforeClass
 	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		
 	}
 
 	@Test
 	public void TC_01_CheckEnvironment() {
+		String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Google");
+		
 	}
+	
 
 	@AfterClass
 	public void afterClass() {
+		driver.quit();
 	}
 
 }
