db1e6038643e8cf40fa020a37f790a6852dbb435
trle <trle@trle-7400.internal.synopsys.com>
2020-07-21 10:16:22 +0700

    - Update TC 01 - Add chromedriver for window

--- src/selenium_api/Topic_01_CheckEnvironment.java
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -3,7 +3,7 @@ package selenium_api;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -17,7 +17,11 @@ public class Topic_01_CheckEnvironment {
 	
 	@BeforeClass
 	public void beforeClass() {
-		driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_win.exe");
+		driver = new ChromeDriver();
+		
+//		driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
