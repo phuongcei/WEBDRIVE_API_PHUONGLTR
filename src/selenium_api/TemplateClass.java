70818c473e3b123ef83cf62f30f39739a628f62d
phuongcei <phuongcei@gmail.com>
2020-08-02 11:26:38 +0700

    - Update firefox driver for template

--- src/selenium_api/TemplateClass.java
+++ src/selenium_api/TemplateClass.java
@@ -4,32 +4,36 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TemplateClass {
 	WebDriver driver;
-	
-	
-  @BeforeClass
-  public void beforeClass() {
-	  	System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_win.exe");
-	  	driver = new ChromeDriver();
-	  	
-	  	driver.get("https://google.com");
-	  	driver.manage().window().maximize();
-	  	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void f() {
-	  
-  }
-  
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+	@BeforeClass
+	public void beforeClass() {
+		// Firefox Driver
+		// driver = new FirefoxDriver();
+
+		// Chrome Driver
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_win.exe");
+		driver = new ChromeDriver();
+
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void f() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
 
 }
--- src/selenium_api/Topic_02_Xpath_Css.java
+++ src/selenium_api/Topic_02_Xpath_Css.java
@@ -4,49 +4,43 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Topic_02_Xpath_Css {
 	WebDriver driver;
-	
-	
-  @BeforeClass
-  public void beforeClass() {
-	  	System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_win.exe");
-	  	driver = new ChromeDriver();
-	  	
-	  	driver.get("https://google.com");
-	  	driver.manage().window().maximize();
-	  	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void Topic_02_Xpath_Css() {
-	  /*
-	   * id
-	   * class
-	   * name
-	   * css
-	   * linkText
-	   * partialLinkText
-	   * tagName
-	   * xpath:    //tagname[@attribute='value'];
-	   * 
-	   */ 
-	 
-	  
-	  driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Le Tran Phuong");
-	  driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
-	  
-	  
-  }
-  
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+	@BeforeClass
+	public void beforeClass() {
+		// Firefox driver
+		driver = new FirefoxDriver();
+
+		// Chrome Driver
+//	  	System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_win.exe");
+//	  	driver = new ChromeDriver();
+
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void Topic_02_Xpath_Css() {
+		/*
+		 * id class name css linkText partialLinkText tagName xpath: //tagname[@attribute='value'];
+		 * 
+		 */
+
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Le Tran Phuong");
+		driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
 
 }
