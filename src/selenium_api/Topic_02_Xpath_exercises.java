f66aac3759ae4a78a902c5a55b99df3ce6a82964
phuongcei <phuongcei@gmail.com>
2020-10-08 23:32:00 +0700

    Topic 2 exercises Part 2

--- src/selenium_api/Topic_02_Xpath_exercises.java
+++ src/selenium_api/Topic_02_Xpath_exercises.java
@@ -1,5 +1,6 @@
 package selenium_api;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -17,8 +18,7 @@ public class Topic_02_Xpath_exercises {
 	public void beforeClass() {
 //		driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver", "./lib/chromedriver");
-		
-		
+
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -97,25 +97,95 @@ public class Topic_02_Xpath_exercises {
 //		* Step 03 - Nhập email invalid: 123434234@12312.12312
 //		* Step 04 - Click Login button
 //		* Step 05 - Verify error message xuất hiện:  Please enter a valid email address. For example johndoe@domain.com.
-		
+
 		driver.get("http://live.demoguru99.com/");
-		
+
 		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
-		
+
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("123434234@12312.123123");
-		
+
 		driver.findElement(By.xpath("//button[@id='send2']")).click();
-		
+
 		Assert.assertEquals(driver.findElement(By.xpath("//div[@id='advice-validate-email-email']")).getText(), "Please enter a valid email address. For example johndoe@domain.com.");
-		
-		
+
 	}
-	
-	
-	
+
+	@Test
+	public void TC_04_loginWithPasswordLessThan6Chars() {
+//		Test Script 04: Login with Password < 6 character
+//		* Step 01 - Truy cập vào trang: http://live.demoguru99.com/
+//		* Step 02 - Click vào link "My Account" để tới trang đăng nhập
+//		* Step 03 - Nhập email correct and password incorrect: automation@gmail.com/ 123
+//		* Step 04 - Click Login button
+//		* Step 05 - Verify error message xuất hiện: Please enter 6 or more characters without leading or trailing spaces.
+		driver.get("http://live.demoguru99.com/");
+
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+
+		driver.findElement(By.id("email")).sendKeys("automation@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("phu");
+
+		driver.findElement(By.xpath("//*[@id='send2']")).click();
+
+		Assert.assertEquals(driver.findElement(By.id("advice-validate-password-pass")).getText(), "Please enter 6 or more characters without leading or trailing spaces.");
+
+	}
+
+	@Test
+	public void TC_05_createAccount() {
+//		Test Script 06: Create an account
+//		* Step 01 - Truy cập vào trang: http://live.demoguru99.com/
+//		* Step 02 - Click vào link "My Account" để tới trang đăng nhập
+//		* Step 03 - Click CREATE AN ACCOUNT button để tới trang đăng kí tài khoản
+//		* Step 04 - Nhập thông tin hợp lệ vào tất cả các field: First Name/ Last Name/ Email Address/ Password/ Confirm Password
+//		    * (Lưu ý: Tạo random cho dữ liệu tại field Email Address)
+//		* Step 05 - Click REGISTER button
+//		* Step 06 - Verify message xuất hiện khi đăng kí thành công: Thank you for registering with Main Website Store.
+//		* Step 07 - Logout khỏi hệ thống
+//		* Step 08 - Kiểm tra hệ thống navigate về Home page sau khi logout thành công
+
+		driver.get("http://live.demoguru99.com/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+		driver.findElement(By.id("firstname")).sendKeys("Phuong");
+		driver.findElement(By.xpath("//input[@id='middlename']")).sendKeys("Tran");
+		driver.findElement(By.id("lastname")).sendKeys("Le");
+		driver.findElement(By.id("email_address")).sendKeys(randomEmail());
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys("123456");
+
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+
+		String registerMsg = driver.findElement(By.xpath("//ul[@class='messages']//span[contains(text(),'Thank you for registering')]")).getText();
+
+		Assert.assertEquals(registerMsg, "Thank you for registering with Main Website Store.");
+
+		driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[text()='Account']")).click();
+		driver.findElement(By.xpath("//div[@id='header-account']//a[@title='Log Out']")).click();
+
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='main']//h2")).getText().trim(), "THIS IS DEMO SITE FOR");
+
+	}
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
+	}
+
+	public String randomEmail() {
+		String email;
+		int max = 1000;
+		int min = 900;
+		String prefix = "auto_email_";
+		int subfix;
+		Random random = new Random();
+		subfix = random.nextInt(max - min) + min;
+
+		email = prefix + subfix + "@gmail.com";
+
+		return email;
 	}
 
 }
