99e1885be74c6110777c032c93403dae82a3b9ed
phuongcei <phuongcei@gmail.com>
2020-11-18 07:43:52 +0700

    - Update Topic 05, commons class

--- src/selenium_api/Commons.java
+++ src/selenium_api/Commons.java
@@ -5,9 +5,11 @@ import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Commons {
 
+	// Generate a random email
 	public static String randomEmail() {
 		String randomMail;
 
@@ -25,10 +27,22 @@ public class Commons {
 		System.out.println("Random email: " + randomMail);
 		return randomMail;
 	}
-	
+
+	// Remove an specific attribute of an element by Javascript executor
 	public static void removeAttributeElement(WebDriver driver, By by, String attributeName) {
-		JavascriptExecutor js = (JavascriptExecutor)driver;
-		js.executeScript("document.querySelector('.fhs-btn-login').removeAttribute(attributeName)");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("arguments[0].removeAttribute('" + attributeName + "')", driver.findElement(by));
+
+		// Reference:
+		// https://stackoverflow.com/questions/53376933/how-to-remove-an-element-attribute-using-selenium-and-c
+
+	}
+
+	// Click an element by Javascript executor
+	public static void clickElementByJavascript(WebDriver driver, WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", element);
 	}
 
 }
--- src/selenium_api/Topic_05_Button_RadioButton_Checkbox_Alert.java
+++ src/selenium_api/Topic_05_Button_RadioButton_Checkbox_Alert.java
@@ -1,5 +1,7 @@
 package selenium_api;
 
+import static org.testng.Assert.assertTrue;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +11,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,6 +20,7 @@ import org.testng.annotations.Test;
 
 public class Topic_05_Button_RadioButton_Checkbox_Alert {
 	WebDriver driver;
+	WebDriverWait wait;
 	JavascriptExecutor jsExecutor;
 
 	@BeforeClass
@@ -33,11 +38,11 @@ public class Topic_05_Button_RadioButton_Checkbox_Alert {
 //		driver.manage().window().maximize();  //-> From Chrome 70, this is an issue.
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
+		wait = new WebDriverWait(driver, 30);
 		jsExecutor = (JavascriptExecutor) driver;
 	}
 
-	@Test
-	public void TC01_handleButtonJs() {
+	public void TC01_handleButtonJs() throws Exception {
 
 		driver.get("https://www.fahasa.com/customer/account/create");
 		driver.findElement(By.xpath("//ul[@id='popup-login-tab_list']//a")).click();
@@ -56,18 +61,56 @@ public class Topic_05_Button_RadioButton_Checkbox_Alert {
 		driver.findElement(By.xpath("//ul[@id='popup-login-tab_list']//a")).click();
 
 		// Remove thuoc tinh disable cua button
+		By buttonXpathBy = By.xpath("//button[@class='fhs-btn-login']");
 
-		jsExecutor.executeScript("document.querySelector('.fhs-btn-login').removeAttribute('disabled')");
-		
+		Commons.removeAttributeElement(driver, buttonXpathBy, "disabled");
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//button[@class='fhs-btn-login']")).click();
-		
+
 		List<WebElement> listMessages = driver.findElements(By.xpath("//div[@class='popup-login-content']//div[@class='fhs-input-alert']"));
-		for(WebElement massage : listMessages) {
+		for (WebElement massage : listMessages) {
 			Assert.assertEquals(massage.getText().trim(), "Thông tin này không thể để trống");
 		}
-		
-		
 
+	}
+
+	@Test
+	public void TC02_handleDefaultCheckboxRadioButton() throws Exception {
+//			Step 01 - Truy cập trang: http://demos.telerik.com/kendo-ui/styling/checkboxes
+		driver.get("http://demos.telerik.com/kendo-ui/styling/checkboxes");
+
+//			Step 02 - Click vào checkbox: Dual-zone air conditioning
+		WebElement dualZoneCheckbox = driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']//preceding-sibling::input"));
+
+		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@id='example']//li")));
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", dualZoneCheckbox);
+		dualZoneCheckbox.click();
+		Thread.sleep(3000);
+
+//			Step 03 - Kiểm tra checkbox đó đã chọn
+		Assert.assertTrue(dualZoneCheckbox.isSelected());
+
+//			Step 04 - Sau khi checkbox đã được chọn - bỏ chọn nó và kiểm tra nó chưa được chọn
+		dualZoneCheckbox.click();
+		Thread.sleep(3000);
+		Assert.assertFalse(dualZoneCheckbox.isSelected());
+
+//			Step 05 - Truy cập vào trang http://demos.telerik.com/kendo-ui/styling/radios
+		driver.get("http://demos.telerik.com/kendo-ui/styling/radios");
+
+//			Step 06 - Click vào radio button: 2.0 Petrol, 147kW
+		WebElement petrolRadio = driver.findElement(By.xpath("//div[@id='example']//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input"));
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", petrolRadio);
+		petrolRadio.click();
+		Thread.sleep(3000);
+
+//			Step 07 - Kiểm tra radio button đó đã chọn hay chưa/ nếu chưa chọn lại
+		if (petrolRadio.isSelected()) {
+			System.out.println("Radio button is selected.");
+		} else {
+			System.out.println("Radio button is NOT selected. Re-select.");
+			petrolRadio.click();
+		}
 	}
 
 	@AfterClass
